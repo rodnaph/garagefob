aada405370299d040de4307a13580931e99ed22b
rod <rod@pu-gh.com>
2012-08-25 11:03:03 +0100

    added open route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 
-exports.index = function(req, res){
+exports.enter = function(req, res)
+{
+  // if code matches
+  res.render('opening', { title: 'Garage Opening' })
+};
+
+exports.index = function(req, res)
+{
   res.render('index', { title: 'Garage Fob' });
 };
 
